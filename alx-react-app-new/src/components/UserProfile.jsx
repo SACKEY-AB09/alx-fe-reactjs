17f1b514cef406af2815c17537c2a507c83fcac6
sackey <sackeyabraham67@gmail.com>
2025-03-19 00:15:21 +0000

    added the padding tag

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
     return(
-        <div>
-            <h2 style={{ color:'blue', border:'1px solid gray', margin:'10px',backgroundColor:"rgb(22, 129, 120)"}}>Name: {props.name}</h2>
+        <div style={{ border:'1px solid gray', padding:'10px', margin:'10px',backgroundColor:"rgb(22, 129, 120)"}} >
+            <h2 style={{color:'blue'}}>Name: {props.name}</h2>
             <p> Age:<span style={{fontWeight:'bold'}}>{props.age} </span></p>
             <p style ={{border:'1px solid grey'}}>Bio: {props.bio}</p>
         </div>
