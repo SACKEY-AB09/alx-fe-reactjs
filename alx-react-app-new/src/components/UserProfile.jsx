95238878d97472d51017c72ba79363cc564865e4
sackey <sackeyabraham67@gmail.com>
2025-03-19 00:00:14 +0000

    added span tag

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ const UserProfile = (props) => {
     return(
         <div>
             <h2 style={{ color:'blue', border:'1px solid gray', margin:'10px',backgroundColor:"rgb(22, 129, 120)"}}>Name: {props.name}</h2>
-            <p style={{fontWeight:'bold'}}>Age: {props.age}</p>
+            <p> Age:<span style={{fontWeight:'bold'}}>{props.age} </span></p>
             <p style ={{border:'1px solid grey'}}>Bio: {props.bio}</p>
         </div>
     );
