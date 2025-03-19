d4043bf0528c7dd693c9dd2324260ec43c21cb91
sackey <sackeyabraham67@gmail.com>
2025-03-19 01:13:28 +0000

    added my counter app to react

--- alx-react-app-new/src/main.jsx
+++ alx-react-app-new/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import UserProfile from './components/UserProfile'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import App from './App'
 import Footer from './components/Footer'
 
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
     <UserProfile />
     <Header />
     <MainContent />
+    <App />
     <Footer />
   </StrictMode>,
 )
